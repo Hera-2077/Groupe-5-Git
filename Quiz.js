b7522cecdd55b7bd3fcb2a885baa37ccfce7f613
Kuzan <ralphsantiago.memong@gmail.com>
2024-10-30 05:26:27 +0100

    Modifications personnel

---
+++ Quiz.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Quelle est la syntaxe correcte pour afficher du texte dans la console en C++ ?",
+        answer: "D. cout << texte",
+        options: [
+            "A. Console.print(texte)",
+            "B. Console.write(texte)",
+            "C. cin << texte",
+            "D. cout << texte"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Quelle est la méthode utilisée pour convertir une chaîne de caractères en entier en C++ ?",
+        answer: "A. stoi()",
+        options: [
+            "A. stoi()",
+            "B. toInt()",
+            "C. parseInteger()",
+            "D. convertToInt()"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Quelle est la classe utilisée pour lire des données à partir d'un fichier en C++ ?",
+        answer: "C. ifstream",
+        options: [
+            "A. File",
+            "B. Reader",
+            "C. ifstream",
+            "D. FileReader"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Quelle est la méthode utilisée pour ajouter un élément à la fin d'un vecteur en C++ ?",
+        answer: "B. push_back()",
+        options: [
+            "A. addFirst()",
+            "B. push_back()",
+            "C. append()",
+            "D. addLast()"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Quelle est la méthode utilisée pour trier un tableau en C++ ?",
+        answer: "A. std::sort()",
+        options: [
+            "A. std::sort()",
+            "B. std::arrange()",
+            "C. std::order()",
+            "D. std::Throw()"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Quelle est la classe utilisée pour gérer les exceptions en C++ ?",
+        answer: "D. std::exception",
+        options: [
+            "A. Exception",
+            "B. Error",
+            "C. Throwable",
+            "D. std::exception"
+        ]
+    }, 
+    {
+        numb: 7,
+        question: "Quelle est la classe utilisée pour représenter une collection d'éléments uniques en C++ ?",
+        answer: "D. unordered_set",
+        options: [
+            "A. Set",
+            "B. List",
+            "C. Map",
+            "D. unordered_set"
+        ]
+    }, 
+    {
+        numb: 8,
+        question: "Quelle est la méthode utilisée pour vérifier si une chaîne de caractères contient une sous-chaîne en C++ ?",
+        answer: "B. includes()",
+        options: [
+            "A. contains()",
+            "B. includes()",
+            "C. has()",
+            "D. find()"
+        ]
+    }, 
+    {
+        numb: 9,
+        question: "Quel est le véritable fruit du démon de Luffy ?",
+        answer: "C. Hito Hito no mi modèle Nika()",
+        options: [
+            "A. Gomu Gomu no mi modèle Nika()",
+            "B. Nomi Nomi no mi modèle Sungod()",
+            "C. Hito Hito no mi modèle Nika()",
+            "D. Mane Mane no mi modèle Sungod()"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Quelle est la classe utilisée pour créer des threads en C++ ?",
+        answer: "D. std::thread",
+        options: [
+            "A. Thread",
+            "B. cin << Thread",
+            "C. Executor",
+            "D. std::thread"
+        ]
+    }
+]
